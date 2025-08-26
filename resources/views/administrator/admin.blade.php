29a1ceba62f748efda8a167120dfbae4288f6ca3
Miki <mickeytuts2401@gmail.com>
2025-08-26 17:26:49 +0200

    minor change testing seeding on deploy

--- resources/views/administrator/admin.blade.php
+++ resources/views/administrator/admin.blade.php
@@ -3,8 +3,8 @@
         <div class="container">
             <h2>Administrator strana</h2>
         </div>
-        
-        
+
+
     </x-slot:heading>
     <div class="container mx-auto mt-6">
         <div class="bg-white p-6 rounded shadow mb-6">
@@ -14,7 +14,7 @@
                 <x-basic-link href="/radnici">Svi radnici</x-basic-link>
                 <x-basic-link href="/ponude_dobavljaca">Sve ponude dobavljaca</x-basic-link>
                 <x-basic-link href="/proizvodi">Svi proizvodi</x-basic-link>
-                
+
             </div>
         </div>
     </div>
